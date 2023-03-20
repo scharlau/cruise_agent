316f2f962aa4359f31ffd12c6dfffa085fb86009
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2023-03-20 09:55:23 +0000

    added fixtures and tests

--- cruises/tests.py
+++ cruises/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Ship,Cruise, Cabin
 
 # Create your tests here.
+class CruiseTests(TestCase):
+    fixtures =['cruises']
+
+    def test_index(self):
+        client = Client()
+        response = client.get('')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Cruise')
+        self.assertContains(response, 'Davenport-Ponce')
